66d0692342ac66f3034bf1b12f9086b8409ad077
HoshinAkoko <nagiawa@foxmail.com>
2024-03-14 17:53:35 +0800

    第一次提交！

---
+++ src/server.py
@@ -0,0 +1,111 @@
+"""
+服务端
+"""
+
+import socket
+import threading
+import util
+
+default_key = "0123456789ABCDEF"
+
+
+# 主业务
+def main_service(params_json):
+    params_dict = util.verify_dict(params_json)
+    if not params_dict:
+        return error_msg("验签失败！")
+
+
+
+# 返回单条错误信息
+def error_msg(msg):
+    error_dict = {
+        "type": -1,
+        "params": {
+            "code": -1,
+            "msg": f"{msg}"
+        }
+    }
+    return error_dict
+
+
+# 处理每个客户端连接
+def handle_client(stop_event, client_socket, addr):
+    print(f"新连接: {addr}")
+    print(f"当前连接数：{len(client_list)}")
+    client_socket.settimeout(3.0)
+    while True:
+        # 接受数据，若空则关闭
+        try:
+            msg = client_socket.recv(1024)
+            if not msg:
+                break
+            msg = msg.decode('utf-8')
+            msg = util.aes_decrypt(msg, key_dict.get(addr) if key_dict.get(addr) is not None else default_key)
+            print(f"接受用户{addr}的信息：{msg}")
+            # 业务处理
+            respond_dict = main_service(msg)
+            respond_dict["timestamp"] = util.get_timestamp
+            respond = util.sign_to_json(respond_dict)
+            respond = util.aes_encrypt(respond, key_dict.get(addr) if key_dict.get(addr) is not None else default_key)
+            respond.encode('utf-8')
+            client_socket.send(respond)
+        except socket.timeout:
+            pass
+        except ConnectionResetError:
+            break
+        except Exception:
+            break
+        finally:
+            if stop_event.is_set():
+                break
+    # 关闭连接
+    client_socket.close()
+    print(f"连接已关闭: {addr}")
+    print(f"当前连接数：{len(client_list)}")
+    client_list.remove(client_socket)
+
+
+# 启动服务器
+def start_server(stop_event, host, port, max_listen):
+    # 创建 socket 对象
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.settimeout(3.0)
+    server_socket.bind((host, port))
+    server_socket.listen(max_listen)
+    print(f"ChatRoom服务端启动于{host}:{port}...")
+    while True:
+        try:
+            # 等待一个新连接
+            client_socket, addr = server_socket.accept()
+            # 为这个连接创建一个新的线程
+            client_thread = threading.Thread(target=handle_client, args=(stop_event, client_socket, addr))
+            client_list.append(client_socket)
+            client_thread.start()
+        except socket.timeout:
+            pass
+        finally:
+            if stop_event.is_set():
+                break
+    server_socket.close()
+    print(f"Socket服务端已关闭")
+
+
+if __name__ == "__main__":
+    HOST = "0.0.0.0"
+    PORT = 8919
+    # start_server(HOST, PORT, 5)
+    # 改为新开线程处理服务器
+    _event = threading.Event()
+    server_thread = threading.Thread(target=start_server, args=(_event, HOST, PORT, 5))
+    client_list = []
+    key_dict = {}
+    server_thread.start()
+    while True:
+        new_command = input()
+        if "exit".upper() == new_command.upper():
+            _event.set()  # 通知线程停止
+            print(f"等待线程退出...")
+            break
+        else:
+            pass
---
+++ src/util.py
@@ -0,0 +1,88 @@
+# 工具函数
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+from datetime import datetime
+import base64
+import hashlib
+import json
+
+
+# 签名并转为json
+def sign_to_json(unsorted_dict, sign_key):
+    unsorted_dict["key"] = f"{sign_key}"
+    sorted_dict = {key: unsorted_dict[key] for key in sorted(unsorted_dict)}
+    key_json = json.dumps(sorted_dict)
+    sign = get_md5(key_json)
+    unsorted_dict.pop("key")
+    unsorted_dict["sign"] = f"{sign}"
+    signed_dict = {key: unsorted_dict[key] for key in sorted(unsorted_dict)}
+    signed_json = json.dumps(signed_dict)
+    return signed_json
+
+
+# 验签并转为dict
+def verify_to_dict(signed_json, sign_key):
+    unsorted_dict = json.loads(signed_json)
+    sign = unsorted_dict.pop("sign")
+    unsorted_dict["key"] = f"{sign_key}"
+    sorted_dict = {key: unsorted_dict[key] for key in sorted(unsorted_dict)}
+    key_json = json.dumps(sorted_dict)
+    verify_sign = get_md5(key_json)
+    if verify_sign == sign:
+        unsorted_dict.pop("key")
+        return unsorted_dict
+    else:
+        return False
+
+
+# 获取md5
+def get_md5(data):
+    md5 = hashlib.md5()  # 创建一个md5对象
+    md5.update(data.encode('utf-8'))  # 使用utf-8编码更新数据
+    return md5.hexdigest().upper()  # 返回十六进制的哈希值
+
+
+# 获取时间戳
+def get_timestamp():
+    now = datetime.now()
+    timestamp_milliseconds = int(now.timestamp() * 1000)
+    return timestamp_milliseconds
+
+
+# AES加密
+def aes_encrypt(data, hex_key):
+    key = bytes.fromhex(hex_key)  # base64.b64decode(base64_key)
+    if len(key) not in [16, 24, 32]:
+        raise ValueError("Key size must be either 16, 24, or 32 bytes.")
+    iv = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted_data = cipher.encrypt(pad(data.encode(), AES.block_size))
+    return base64.b64encode(iv + encrypted_data).decode()
+
+
+# AES解密
+def aes_decrypt(encrypted_data, hex_key):
+    key = bytes.fromhex(hex_key)
+    encrypted_data_bytes = base64.b64decode(encrypted_data)
+    iv = encrypted_data_bytes[:16]
+    encrypted_data_only = encrypted_data_bytes[16:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_data = unpad(cipher.decrypt(encrypted_data_only), AES.block_size).decode()
+    return decrypted_data
+
+
+if __name__ == "__main__":
+    test_dict = {
+        "aaa": "123",
+        "ccc": 321,
+        "bbb": 231
+    }
+    test_json = sign_to_json(test_dict, "sign_key")
+    print(f"signed json: {test_json}")
+    verify_dict = verify_to_dict(test_json, "sign_key")
+    if not verify_dict:
+        print(f"check failed!")
+    else:
+        print(f"check sign success! dict is {verify_dict}")
