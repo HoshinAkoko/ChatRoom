9e97a4a14175ac378feb6e43527742ae452a1b85
Re50Na <hoshimikiyori@gmail.com>
2024-03-15 12:09:00 +0800

    第一次上传客户端 修改路径分开放置服务端，客户端，工具库文件

--- src/client/main.py
+++ src/client/main.py
@@ -1,7 +1,21 @@
 import txt_reader
 import socket
 import threading
-import input_process
+
+
+# 解析输入切片到list中
+def analyser(ipt_str):
+    if ipt_str == "":
+        return ["msg", ""]
+    if ipt_str[0] == "/":
+        ipt_str = ipt_str.strip()
+        ipt_str = ipt_str[1:]
+        ipt_listed = ipt_str.split(" ")
+    else:
+        ipt_listed = ["msg", ipt_str]
+    return ipt_listed
+
+
 # 解析命令内容并分流到对应函数
 def execute(cmd_list):
     match cmd_list[0]:
@@ -16,6 +30,8 @@ def execute(cmd_list):
             connection_test(cmd_list)
         case "disconnect" | "disconn" | "dc":
             disconnect()
+
+
 # 连接到服务器
 def connect(cmd_list):
     if len(cmd_list) == 1:
@@ -35,23 +51,21 @@ def connect(cmd_list):
     global socket_client
     socket_client = socket.socket()
     socket_client.connect((ip, int(port)))
+
+
 # 用于向服务器发送输入的消息
 def connection_test(cmd_list):
     send_msg = cmd_list[1].encode("utf-8")
     socket_client.send(send_msg)
+
+
 # 断开连接
 def disconnect():
     socket_client.close()
     print("已断开连接")
 
 
-
-
-
-
-
-
 # 启动命令
 while True:
-    listed_input = input_process.analyser(input())
+    listed_input = analyser(input())
     execute(listed_input)
